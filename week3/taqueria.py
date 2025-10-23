03d245e7c1411b60a0653edbdb36de72412e179d
Parmis <159687328+parmisvosoghi1235@users.noreply.github.com>
2025-10-23 13:55:33 +0330

    taqueria.py

--- week3/taqueria.py
+++ week3/taqueria.py
@@ -1,5 +1,5 @@
 menu = {
-    "Baja Taco": 4.00,
+    "Baja Taco": 4.25,
     "Burrito": 7.50,
     "Bowl": 8.50,
     "Nachos": 11.00,
@@ -12,21 +12,16 @@ menu = {
 
 
 def main():
-    """Welcome to Filipe's, may I take your order?"""
-    orders = 0
+    total = 0.0
     while True:
-        # Make user input case insensitive and print total
         try:
             item = input("Item: ").strip().title()
             if item in menu:
-                orders += menu[item]
-                print(f"${orders:.2f}")
-        # Catch CTRL+D to end the session
-        except (EOFError, KeyError):
-            print("", end="\n")
-            quit()
+                total += menu[item]
+                print(f"Total: ${total:.2f}")
+        except EOFError:
+            print(f"\nTotal: ${total:.2f}")
+            break
 
 
 main()
-
-#parmis vosoughi
